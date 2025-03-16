6cd3816f8a2e970f2109908db51fb9fc527fc43e
Viktoryia Shypitsa <shypitsaviktoryia@gmail.com>
2025-03-15 21:38:39 -0700

    Fix build configuration

--- server.js
+++ server.js
@@ -22,9 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+  }
 
 // Connect to the Mongo Atlas DB
 const uri = process.env.ATLAS_URI;
